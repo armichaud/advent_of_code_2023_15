1b30f4c4b33d9b4896447dcec9b5845db8ebe8e2
armichaud <email@alexandermichaud.tech>
2023-12-15 06:51:40 -0500

    Just have to organize the boxes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::fs::read_to_string;
 
+type Lens = (String, i32);
+type Boxes = &'static [Vec<&'static Lens>; 255];
+
 fn get_sequence(filename: &str) -> Vec<String> {
     read_to_string(filename)
         .unwrap()
@@ -21,7 +24,29 @@ fn solution(filename: &str) -> i32 {
     sum
 }
 
+fn total_focusing_power(boxes: Boxes) -> i32 {
+    let mut sum = 0;
+    for (box_number, box_) in boxes.iter().enumerate() {
+        for (slot, lens) in box_.iter().enumerate() {
+            sum += (box_number as i32 + 1) * (slot as i32 + 1) * lens.1;
+        }
+    }
+    sum
+}
+
+fn organize_lenses(lenses: &Vec<String>) -> Boxes {
+  // TODO
+}
+
+fn solution_2(filename: &str) -> i32 {
+    let lenses = get_sequence(filename);
+    let boxes: Boxes = organize_lenses(&lenses);
+    total_focusing_power(&boxes)
+}
+
 fn main() {
     assert_eq!(solution("example.txt"), 1320);
     assert_eq!(solution("input.txt"), 520500);
+    assert_eq!(solution_2("example.txt"), 145);
+    assert_eq!(solution_2("input.txt"), 0);
 }
